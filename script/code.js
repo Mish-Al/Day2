aa9f72582bd9be54d280a14b882f8ab7dc217f59
Mish-Al <mtaliep.lifechoices@gmail.com>
2023-05-17 15:13:57 +0200

    test commit

---
+++ script/code.js
@@ -0,0 +1,14 @@
+let firstName = "Mish-Al";
+let lastName = "Taliep"
+
+let person = [firstName + lastName];
+console.log(person);
+
+// The output is 25
+let numberVar = 5
+console.log(numberVar * 5);
+
+// yes i got an error
+const firsName = 'Taliep'
+firsName = 'Mish-Al'
+console.log(firsName)
